06acb4449937c6bc3b9a487e0fc11135818acf5d
Iniobong <iniobong10000@gmail.com>
2020-03-07 17:46:42 +0100

    Guess Number Game

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+};
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGameRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onErr={err => console.log(err)}
+      />
+    );
+  }
+
+  const configureNewGameHandler = () => {
+    setGameRounds(0);
+    setUserNumber(null);
+  };
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+    setGameRounds(0);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGameRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNum={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess A Number" />
+      {content}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
